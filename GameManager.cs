e481d770f65b943d8804b42c25d1b610e5b643a0
MarkKh <85799327+MarkKh@users.noreply.github.com>
2021-09-22 23:50:29 +0700

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static int nScore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nScore = 0;
+    }
+    void update()
+    {
+
+    }
+}
